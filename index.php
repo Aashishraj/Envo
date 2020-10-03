b69bb150292c52d2e042b5d94884f941ae10e109
Aashish Raj <47362045+Aashishraj@users.noreply.github.com>
2020-10-03 17:07:39 +0530

    database changed

--- index.php
+++ index.php
@@ -140,9 +140,10 @@ if(isset($_POST['submit']))
 	}
 	else{
 	session_start();
-	$con=mysqli_connect("localhost","root","");
-	if(!$con){die('could not connectr'.mysqli_error($con));}
-	mysqli_select_db($con,"envo");
+	$con = mysqli_connect("db4free.net", "enovquest", "envo1234");  
+			 if(!$con)
+			 {die('could not connectr'.mysqli_error($con));}
+			 mysqli_select_db($con,"envoquest");
 
 
 
